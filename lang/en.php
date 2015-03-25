b33039d70debd45e15a3f2bf5c6c5e35b20faa39
David Monllao <davidm@moodle.com>
2015-03-25 22:46:03 +0800

    Translations for EN and ES

---
+++ lang/en.php
@@ -0,0 +1,29 @@
+<?php
+
+// English is the only language with base ./ as it is in the root dir.
+$i18n['{{base}}'] = './';
+$i18n['{{langid}}'] = 'en';
+
+$i18n['{{metadescription}}'] = 'Eat this one. Share cooking specialties with your colleagues or classmates at lunch time.';
+$i18n['{{metakeywords}}'] = 'Eat,lunchbox,lunch,share,food,meal,colleagues,classmates';
+$i18n['{{togglenav}}'] = 'Toggle navigation';
+$i18n['{{about}}'] = 'About';
+$i18n['{{downloads}}'] = 'Downloads';
+$i18n['{{contact}}'] = 'Contact';
+$i18n['{{shortdesc}}'] = 'Share cooking specialties with your colleagues or classmates at lunch time.';
+$i18n['{{subshortdesc}}'] = 'Who cooks today?';
+$i18n['{{about1}}'] = 'Forget about instant noodles, paying for junk food, cooking every evening or eating the same lunchbox every weekday.';
+$i18n['{{about2}}'] = 'Fight to reach the top of your group\'s culinary ranking delighting your colleagues palates.';
+$i18n['{{about3}}'] = 'Receive notifications to your mobile when a colleague or classmate cooks a new dish and share your own with them. Who will be the next master chef?';
+$i18n['{{downloads1}}'] = 'Download&nbsp;<b>Eat this one</b>, create a group and invite your colleagues or classmates.';
+$i18n['{{contactsupport}}'] = 'Support and feedback';
+$i18n['{{contactsupport1}}'] = 'Send us an email with your questions, suggestions or complains; we will be glad to hear from you and help you out.';
+$i18n['{{aroundtheweb}}'] = 'Around the web';
+$i18n['{{opensource}}'] = 'Open Source';
+$i18n['{{technicalstack}}'] = 'All the Eat this one technical stack, from this website to the mobile apps and the backend is based on open source technologies and is available in our <a href="http://github.com/eat-this-one" target="_blank">github account</a>.';
+$i18n['{{androidmodal}}'] = 'While the app is still not available in Google Play you can download and install it from <a href="http://eat-this-one.com/android.apk">here</a>.';
+$i18n['{{androidsupported}}'] = 'Supported platforms:&nbsp;<strong>Android 4.0 onwards</strong>';
+$i18n['{{androiddate}}'] = 'Date:&nbsp;<strong>March 2015</strong>';
+$i18n['{{close}}'] = 'Close';
+$i18n['{{iosmodal}}'] = 'The iOS app is still in development stages, <a href="http://signup.eat-this-one.com" target="_blank">sign up</a> for our project updates list if you are interested in its progress.';
+$i18n['{{iosmodaldate}}'] = 'Date:&nbsp;<strong>March 2015</strong>';
---
+++ lang/es.php
@@ -0,0 +1,28 @@
+<?php
+
+$i18n['{{base}}'] = '../';
+$i18n['{{langid}}'] = 'es';
+
+$i18n['{{metadescription}}'] = 'Eat this one. Comparte comida con tus compañeros de trabajo o de clase.';
+$i18n['{{metakeywords}}'] = 'Comer,fiambrera,comida,cena,compartir,comañeros,trabajo,clase';
+$i18n['{{togglenav}}'] = 'Cambiar navegación';
+$i18n['{{about}}'] = 'Sobre';
+$i18n['{{downloads}}'] = 'Descargas';
+$i18n['{{contact}}'] = 'Contacto';
+$i18n['{{shortdesc}}'] = 'Comparte comida con tus compañeros de trabajo o de clase.';
+$i18n['{{subshortdesc}}'] = 'Quien cocina hoy?';
+$i18n['{{about1}}'] = 'Olvidate de pagar por comida basura, de cocinar cada dia o llevarte la misma fiambrera al trabajo cada dia.';
+$i18n['{{about2}}'] = 'Lucha por el primer puesto en el ranking de especialidades culinarias de tu grupo.';
+$i18n['{{about3}}'] = 'Recibe notificaciones en tu movil cuando un colega cocina un nuevo plato y comparte tus propios platos. Quien será el siguiente master chef?';
+$i18n['{{downloads1}}'] = 'Descarga&nbsp;<b>Eat this one</b>, crea un grupo y invita a tus compañeros de trabajo o de clase.';
+$i18n['{{contactsupport}}'] = 'Soporte y feedback';
+$i18n['{{contactsupport1}}'] = 'Envianos un correo con tus preguntas, sugerencias o quejas; estaremos contentos de saber de ti aunque sea para quejarte.';
+$i18n['{{aroundtheweb}}'] = 'En la red';
+$i18n['{{opensource}}'] = 'Código abierto';
+$i18n['{{technicalstack}}'] = 'Todo el software en el que está basado Eat this one, desde las aplicaciones de movil hasta el servidor es de código abierto y está disponible en nuestra <a href="http://github.com/eat-this-one" target="_blank">cuenta de github</a>.';
+$i18n['{{androidmodal}}'] = 'La app aún no esta disponible para descargar desde Google Play, mientras tanto la puedes descargar desde <a href="http://eat-this-one.com/android.apk">aquí</a>.';
+$i18n['{{androidsupported}}'] = 'Soporte:&nbsp;<strong>Android 4.0 y versiones superiores</strong>';
+$i18n['{{androiddate}}'] = 'Fecha:&nbsp;<strong>Marzo 2015</strong>';
+$i18n['{{close}}'] = 'Cerrar';
+$i18n['{{iosmodal}}'] = 'La app de iOS aun está en fase de desarrollo, <a href="http://signup.eat-this-one.com" target="_blank">apúntate</a> a nuestra lista para recibir las últimas noticias sobre su progreso.';
+$i18n['{{iosmodaldate}}'] = 'Fecha:&nbsp;<strong>Marzo 2015</strong>';
